414ff6fe4eb914fbb9373b46ea9c2fb8ef8f72eb
leavitt <levygsg@hotmail.com>
2021-10-05 01:50:10 -0300

    ECG generator in microcontroller to serial monitor

---
+++ ESP32/ECG_generator/include/main.h
@@ -0,0 +1,149 @@
+/**
+ * @file main.h
+ * @brief 
+ *
+ * @author Levy Gabriel da S. G.
+ * @date October 4 2021
+ */
+
+#ifndef _MAIN_H_ 
+#define _MAIN_H_
+
+/*
+ * Include section
+ * --------------------
+ * Importing all necessary libraries for the good maintenance of the code
+ */
+
+#ifndef C_POSIX_LIB_INCLUDED
+    #define C_POSIX_LIB_INCLUDED
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <sys/unistd.h>
+    #include <sys/stat.h>
+#endif //C_POSIX_LIB_INCLUDED
+
+#ifndef ESP_MANAGEMENT_LIBS_INCLUDED
+    #define ESP_MANAGEMENT_LIBS_INCLUDED
+    #include "esp_err.h" // error codes and helper functions
+    #include "esp_log.h" // logging library
+    #include "esp_vfs_fat.h" // FAT filesystem support
+#endif //ESP_MANAGEMENT_LIBS_INCLUDED
+
+#ifndef DRIVERS_INCLUDED
+    #define DRIVERS_INCLUDED
+    #include "driver/gpio.h"
+    #include "driver/periph_ctrl.h"
+    #include "driver/ledc.h"
+    #include "driver/dac.h"
+#endif //DRIVERS_INCLUDED
+
+#ifndef FREERTOS_LIB_INCLUDED
+    #define FREERTOS_LIB_INCLUDED
+    #include "freertos/FreeRTOS.h"
+    #include "freertos/task.h"
+    #include "freertos/queue.h"
+    #include "freertos/event_groups.h"
+#endif //FREERTOS_LIB_INCLUDED
+
+
+/*
+ * Define section
+ * --------------------
+ * Definition of macros to be used globally in the code
+ */
+
+// pins
+#define BTN_CHANGE_PATHOLOGY  GPIO_NUM_0 // gpio 0  - button
+#define ESP_INTR_FLAG_DEFAULT 0
+
+// buffers
+#define BUFFER_LEN    1024
+#define NUM_BUFFERS   30
+
+// gpio
+#define GPIO_INPUT_PIN_SEL1     (1ULL<<BTN_CHANGE_PATHOLOGY)  // | (1ULL<<ANOTHER_GPIO)
+
+// event flags
+
+#define BIT_(shift) (1<<shift)
+
+enum pathologies{NORMAL_RHYTHM, 
+                SINUS_TACHYCARDIA,
+                ATRIAL_FLUTTER,
+                VENTRICULAR_TACHYCARDIA,
+                VENTRICULAR_FLUTTER};
+
+enum events{ENABLE_ECG_GENERATION};
+
+/*
+ * Global variable declaration section
+ * --------------------
+ * Initialize global variables to be used in any part of the code
+ */
+
+// config input pin - button (GPIO0 commanded by BOOT button)
+gpio_config_t in_conf = {
+    .intr_type    = GPIO_INTR_POSEDGE,   // interrupt on rising edge
+    .mode         = GPIO_MODE_INPUT,     // set as input mode
+    .pin_bit_mask = GPIO_INPUT_PIN_SEL1, // bit mask of pins to set (GPIO00)
+    .pull_down_en = 1,                   // enable pull-down mode
+    .pull_up_en   = 0,                   // disable pull-up mode
+};
+
+float inBuffer[BUFFER_LEN];
+float outBuffer[BUFFER_LEN];
+
+// configuration
+float prev_x[4] = {0,0,0.1,0}; // previous x vector
+float act_x[4]; // actual x vector
+float dt=1e-4; // sampling period
+
+// variables initialized to NORMAL_RHYTHM
+const float C=1.35, beta=4;
+float alpha[4] = {-0.024, 0.0216, -0.0012, 0.12};
+float H=3, gammat=7;
+
+// freertos variables
+TaskHandle_t xTaskGENhandle; // handle to ECG generation task
+TaskHandle_t xTaskOUThandle;  // handle to ECG output task
+QueueHandle_t xQueueData;        // data queue for transfering generated data to DAC output
+EventGroupHandle_t xPathologies; // event group to signalize which pathology must be simulated
+EventGroupHandle_t xEvents;      // event group to manage tasks context
+
+portMUX_TYPE spinlock = portMUX_INITIALIZER_UNLOCKED;
+
+/*
+ * Function prototype section
+ * --------------------
+ * Initialize functions prototypes to later be defined
+ */
+
+/**
+ * @brief Function responsible to change parameters in ECG signal generation so multiple pathologies can be simulated
+ */
+void change_pathology();
+
+/**
+ * @brief Task to generate ECG signal with Queiroz-Juárez et al., 2019 algorithm (https://doi.org/10.1038/s41598-019-55448-5).
+ *
+ * @param pvParameters freeRTOS task parameters
+ */
+void vTaskGenerateECG(void * pvParameters);
+
+/**
+ * @brief Task to output ECG signal previous buffered by generator task.
+ *
+ * @param pvParameters freeRTOS task parameters
+ */
+void vTaskOutputECG(void * pvParameters);
+
+
+/**
+ * @brief Interrupt service routine for button pressed (associated with BOOT button a.k.a GPIO0)
+ */
+static void IRAM_ATTR ISR_BTN();
+
+
+#endif //_MAIN_H_
---
+++ ESP32/ECG_generator/src/main.c
@@ -0,0 +1,202 @@
+/**
+ * @file main.c
+ * @brief 
+ *
+ * @author Levy Gabriel da S. G.
+ * @date October 4 2021
+ */
+
+#include "main.h"
+
+/*
+ * Main section
+ * --------------------
+ * Main function with general single time configuration in BOOT time
+ */
+
+
+void app_main(void)
+{  
+    BaseType_t xReturnedTask[2];
+
+    // configure gpio pins
+    ESP_ERROR_CHECK(gpio_config(&in_conf));                               // initialize input pin as pathology changer
+    ESP_ERROR_CHECK(gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT));     // install gpio isr service
+    ESP_ERROR_CHECK(gpio_isr_handler_add(BTN_CHANGE_PATHOLOGY, ISR_BTN, NULL));  // hook isr handler for specific gpio pin
+
+    // create queue/semaphores/event groups
+    xQueueData        = xQueueCreate(NUM_BUFFERS,BUFFER_LEN*sizeof(float)); // 32 bits = 4 bytes
+    xEvents           = xEventGroupCreate();
+    xPathologies      = xEventGroupCreate();
+    
+    if(xQueueData == NULL){ // tests if queue creation fails
+        ESP_LOGE("app_main", "Failed to create data queue.\n");
+        while(1);
+    }
+    if((xEvents == NULL) || (xPathologies == NULL)){ // tests if event group creation fails
+        ESP_LOGE("app_main", "Failed to create event groups.\n");
+        while(1);
+    }
+
+    // create tasks
+    xReturnedTask[0] = xTaskCreatePinnedToCore(vTaskGenerateECG, "taskGEN", 2048, NULL, configMAX_PRIORITIES-1, &xTaskGENhandle, APP_CPU_NUM);
+    xReturnedTask[1] = xTaskCreatePinnedToCore(vTaskOutputECG,   "taskOUT", 2048, NULL, configMAX_PRIORITIES-2, &xTaskOUThandle, PRO_CPU_NUM);
+    
+    for(int itr=0; itr<2; itr++) // iterate over tasks 
+    {
+        if(xReturnedTask[itr] == pdFAIL){ // tests if task creation fails
+            ESP_LOGE("app_main", "Failed to create task %d.\n", itr);
+            while(1);
+        }
+    }
+
+    // set flag informing that the recording is stopped
+    xEventGroupSetBits(xEvents, BIT_(ENABLE_ECG_GENERATION));
+
+    ESP_LOGI("app_main", "Successful BOOT!");
+    vTaskDelete(NULL);
+}
+
+/*
+ * freeRTOS section
+ * --------------------
+ * Declaration of freeRTOS tasks
+ */
+   
+void vTaskGenerateECG(void * pvParameters)
+{
+    int i;
+    while(1)
+    {
+        if(xEventGroupWaitBits(xEvents, BIT_(ENABLE_ECG_GENERATION), pdFALSE, pdTRUE, portMAX_DELAY) & BIT_(ENABLE_ECG_GENERATION))
+        {
+            for(i=0; i<BUFFER_LEN; i++)
+            {
+                act_x[0] = gammat*(prev_x[0]-prev_x[1]-C*prev_x[0]*prev_x[1]-prev_x[0]*prev_x[1]*prev_x[1])*dt+prev_x[0];
+                act_x[1] = gammat*(H*prev_x[0]-3*prev_x[1]+C*prev_x[0]*prev_x[1]+prev_x[0]*prev_x[1]*prev_x[1]+beta*(prev_x[3]-prev_x[1]))*dt+prev_x[1];
+                act_x[2] = gammat*(prev_x[2]-prev_x[3]-C*prev_x[2]*prev_x[3]-prev_x[2]*prev_x[3]*prev_x[3])*dt+prev_x[2];
+                act_x[3] = gammat*(H*prev_x[2]-3*prev_x[3]+C*prev_x[2]*prev_x[3]+prev_x[2]*prev_x[3]*prev_x[3]+2*beta*(prev_x[1]-prev_x[3]))*dt+prev_x[3];
+                inBuffer[i] = alpha[0]*act_x[0]+alpha[1]*act_x[1]+alpha[2]*act_x[2]+alpha[3]*act_x[3];
+                prev_x[0] = act_x[0];
+                prev_x[1] = act_x[1];
+                prev_x[2] = act_x[2];
+                prev_x[3] = act_x[3];
+            }
+
+            // enqueue ecg data buffer
+            xQueueSend(xQueueData,&inBuffer,portMAX_DELAY);
+            vTaskDelay(1);
+        } else{vTaskDelay(1);}
+    }
+}
+
+void vTaskOutputECG(void * pvParameters)
+{
+    int i;
+    while(1)
+    {
+        while(xQueueData!=NULL && xQueueReceive(xQueueData, &outBuffer, portMAX_DELAY)==pdTRUE) // wait for data to be read
+        {
+            for(i=0; i<BUFFER_LEN; i++)
+            {
+                printf("%f\n", outBuffer[i]);
+            }
+            vTaskDelay(1);
+        }
+        vTaskDelay(1);
+    }
+}
+
+/*
+ * ISR section
+ * --------------------
+ * Declaration of interrupt service routines for push-buttons, timers, etc.
+ */
+
+static void IRAM_ATTR ISR_BTN()
+{
+    portENTER_CRITICAL_ISR(&spinlock);
+    change_pathology();
+    portEXIT_CRITICAL_ISR(&spinlock);
+}
+
+/*
+ * Auxiliary functions section
+ * --------------------
+ * Declaration of auxiliary functions
+ */
+
+void change_pathology()
+{
+    // change pathology on button click, from previous to next in sequential order
+    if(xEventGroupGetBits(xPathologies) & BIT_(NORMAL_RHYTHM))
+    {
+        xEventGroupClearBits(xPathologies, BIT_(NORMAL_RHYTHM));
+        xEventGroupSetBits(xPathologies, BIT_(SINUS_TACHYCARDIA));
+
+        alpha[0] = 0;
+        alpha[1] = -0.1;
+        alpha[2] = 0;
+        alpha[3] = 0;
+        H = 2.848;
+        gammat = 21;
+    } else if(xEventGroupGetBits(xPathologies) & BIT_(SINUS_TACHYCARDIA))
+    {
+        xEventGroupClearBits(xPathologies, BIT_(SINUS_TACHYCARDIA));
+        xEventGroupSetBits(xPathologies, BIT_(ATRIAL_FLUTTER));
+
+        alpha[0] = -0.068;
+        alpha[1] = 0.028;
+        alpha[2] = -0.024;
+        alpha[3] = 0.12;
+        H = 1.52;
+        gammat = 13;
+    } else if(xEventGroupGetBits(xPathologies) & BIT_(ATRIAL_FLUTTER))
+    {   
+        xEventGroupClearBits(xPathologies, BIT_(ATRIAL_FLUTTER));
+        xEventGroupSetBits(xPathologies, BIT_(VENTRICULAR_TACHYCARDIA));
+
+        alpha[0] = 0;
+        alpha[1] = 0;
+        alpha[2] = 0;
+        alpha[3] = -0.1;
+        H = 2.178;
+        gammat = 21;
+    } else if(xEventGroupGetBits(xPathologies) & BIT_(VENTRICULAR_TACHYCARDIA))
+    {
+        xEventGroupClearBits(xPathologies, BIT_(VENTRICULAR_TACHYCARDIA));
+        xEventGroupSetBits(xPathologies, BIT_(VENTRICULAR_FLUTTER));
+
+        alpha[0] = 0.1;
+        alpha[1] = -0.02;
+        alpha[2] = -0.01;
+        alpha[3] = 0;
+        H = 2.178;
+        gammat = 13;
+    } else if(xEventGroupGetBits(xPathologies) & BIT_(VENTRICULAR_FLUTTER))
+    {
+        xEventGroupClearBits(xPathologies, BIT_(VENTRICULAR_FLUTTER));
+        xEventGroupSetBits(xPathologies, BIT_(NORMAL_RHYTHM));
+
+        alpha[0] = -0.024;
+        alpha[1] = 0.0216;
+        alpha[2] = -0.0012;
+        alpha[3] = 0.12;
+        H = 3;
+        gammat = 7;
+    } else
+    {
+        xEventGroupSetBits(xPathologies, BIT_(NORMAL_RHYTHM));
+
+        alpha[0] = -0.024;
+        alpha[1] = 0.0216;
+        alpha[2] = -0.0012;
+        alpha[3] = 0.12;
+        H = 3;
+        gammat = 7;
+    }
+    prev_x[0] = 0;
+    prev_x[1] = 0;
+    prev_x[2] = 0.1;
+    prev_x[3] = 0;
+}
